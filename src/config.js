615bd35c5f3ebed90482596e6cb801fd4fa3f659
Jordan Epps <jordanepps@gmail.com>
2019-09-12 14:31:51 -0400

    Set up migration

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-	PORT: process.env.PORT || 8000,
-	NODE_ENV: process.env.NODE_ENV || 'deployment'
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'deployment',
+  DB_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://jordanepps@localhost/smartphonemedic-test',
+  JWT_SECRET:
+    process.env.JWT_SECRET ||
+    'F42623B55ACEDEA18D61C898DAAD7135917F6854DF869305458F6D9D17BCBADA',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '24h'
 };
