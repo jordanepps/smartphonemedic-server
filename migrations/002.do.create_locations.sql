0a9777c7d7494cc85ddbd70ebe15a9da5b69b8e9
Jordan Epps <jordanepps@gmail.com>
2019-09-12 15:51:09 -0400

    Add color and location table to migration

---
+++ migrations/002.do.create_locations.sql
@@ -0,0 +1,5 @@
+CREATE TABLE locations
+(
+    id SERIAL PRIMARY KEY,
+    location_name TEXT NOT NULL
+);
---
+++ migrations/003.do.create_colors.sql
@@ -0,0 +1,5 @@
+CREATE TABLE colors
+(
+    id SERIAL PRIMARY KEY,
+    color_name TEXT NOT NULL
+);
